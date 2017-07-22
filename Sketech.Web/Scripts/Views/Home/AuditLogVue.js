6760ee6d1dfeb5d7acec8853a02f4599a389393a
Nichos <nichos@dx1app.com>
2017-07-22 14:28:43 +0700

    Add VueJs sample page

---
+++ Sketech.Web/Scripts/Views/Home/AuditLogVue.js
@@ -0,0 +1,22 @@
+﻿var apiURL = '/api/logging/GetAuditLogs';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        logs: []
+    },
+    created: function () {
+        this.fetchData();
+    },
+    methods: {
+        fetchData: function () {
+            var xhr = new XMLHttpRequest();
+            var self = this;
+            xhr.open('GET', apiURL);
+            xhr.onload = function () {
+                self.logs = JSON.parse(xhr.responseText);
+            }
+            xhr.send();
+        }
+    }
+})
---
+++ Sketech.Web/apis/LoggingController.cs
@@ -0,0 +1,31 @@
+﻿using Sketech.Dals.Logging;
+using Sketech.Entities.Logging;
+using Sketech.Infrastructure.Ioc;
+using Sketech.Infrastructure.Logging;
+using Sketech.Web.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Sketech.Web.apis
+{
+    public class LoggingController : ApiController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IList<AuditLogEntry>> GetAuditLogs(int skip = 0, int take = 100)
+        {
+            IList<AuditLogEntry> logs = null;
+            var logger = SkServiceLocator.Get<SkLoggerBase>() as SkDbLogger;
+            if (logger != null)
+            {
+                logs = await logger.GetAuditLogs();
+            }
+            return logs;
+        }
+    }
+}
