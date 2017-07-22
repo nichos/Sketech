96b30ff7b08fdb82603fa3be3028945a421f8ef3
Nichos <nichos@dx1app.com>
2017-07-22 14:45:11 +0700

    Change logging controller to have skAuthorize attribute

--- Sketech.Web/apis/LoggingController.cs
+++ Sketech.Web/apis/LoggingController.cs
@@ -3,11 +3,7 @@ using Sketech.Entities.Logging;
 using Sketech.Infrastructure.Ioc;
 using Sketech.Infrastructure.Logging;
 using Sketech.Web.Attributes;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -16,7 +12,7 @@ namespace Sketech.Web.apis
     public class LoggingController : ApiController
     {
         [HttpGet]
-        [AllowAnonymous]
+        [SkAuthorize]
         public async Task<IList<AuditLogEntry>> GetAuditLogs(int skip = 0, int take = 100)
         {
             IList<AuditLogEntry> logs = null;
